d0c4b17440b1c3af2ef569912c35adf549494f9a
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-11-25 23:27:31 +0900

    feat(FileApiController.java): add FileApiController with requestMapping

---
+++ src/main/java/com/efub/clone/googledrive/controller/FileApiController.java
@@ -0,0 +1,13 @@
+package com.efub.clone.googledrive.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("api")
+public class FileApiController {
+
+}

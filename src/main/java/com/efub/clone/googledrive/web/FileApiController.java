4fc52a1f2aeb0f26f74cf4e66573ea2cb6891f63
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-11-27 05:17:20 +0900

    feat(FileApiController.java): add download and delete apis

--- src/main/java/com/efub/clone/googledrive/web/FileApiController.java
+++ src/main/java/com/efub/clone/googledrive/web/FileApiController.java
@@ -1,21 +1,23 @@
 package com.efub.clone.googledrive.web;
 
 import com.efub.clone.googledrive.service.FileService;
+import com.efub.clone.googledrive.service.S3Service;
 import com.efub.clone.googledrive.web.dto.FileRequestDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
 public class FileApiController {
     private final FileService fileService;
+    private final S3Service s3Service;
 
     @PostMapping("/files")
     @ResponseStatus(HttpStatus.CREATED)
@@ -27,6 +29,29 @@ public class FileApiController {
             FileRequestDto requestDto = new ObjectMapper().readValue(requestDtoString, FileRequestDto.class);
             return ResponseEntity.ok().body(fileService.uploadFile(file, requestDto));
         }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("fail");
+        }
+    }
+
+
+    @GetMapping("/files/{fileName}/download")
+    public ResponseEntity<byte[]> downloadFile(
+        @RequestParam(value = "fileName", required = true) String fileName
+    ) throws IOException {
+        return s3Service.getObject(fileName);
+    }
+
+
+    @DeleteMapping("/files/{fileName}/delete")
+    public ResponseEntity<Object> deleteFile(
+        @RequestParam(value = "fileName", required = true) String fileName,
+        @RequestParam(value = "requestDto") String requestDtoString)
+    {
+        try {
+            FileRequestDto requestDto = new ObjectMapper().readValue(requestDtoString, FileRequestDto.class);
+            return ResponseEntity.ok().body(fileService.deleteFile(fileName));
+        } catch(Exception e){
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("fail");
         }

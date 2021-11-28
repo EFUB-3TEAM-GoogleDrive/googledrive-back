28f51def427f6c3d16c5c2aeec122b7e0fc79739
hak2711 <hak271111@gmail.com>
2021-11-28 20:11:47 +0900

    feat(UserController,Service): add getDeleted files, folders

---
+++ src/main/java/com/efub/clone/googledrive/web/UserApiController.java
@@ -0,0 +1,27 @@
+package com.efub.clone.googledrive.web;
+
+import com.efub.clone.googledrive.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class UserApiController {
+    private final UserService userService;
+
+    @GetMapping("users/{userId}/deleted")
+    public ResponseEntity<Object> getDeleted(@PathVariable Long userId)
+    {
+        try{
+            return ResponseEntity.ok().body(userService.getDeleted(userId));
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("invalid user");
+        }
+    }
+}

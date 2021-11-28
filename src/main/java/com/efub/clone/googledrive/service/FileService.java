43e7652609b5390d55baf65321859d58992f7ed1
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-11-29 03:17:53 +0900

    feat(FileService.java): add moveFolder and error checking method to refactor

--- src/main/java/com/efub/clone/googledrive/service/FileService.java
+++ src/main/java/com/efub/clone/googledrive/service/FileService.java
@@ -2,8 +2,11 @@ package com.efub.clone.googledrive.service;
 
 import com.efub.clone.googledrive.domain.file.File;
 import com.efub.clone.googledrive.domain.file.FileRepository;
+import com.efub.clone.googledrive.domain.folder.Folder;
+import com.efub.clone.googledrive.domain.folder.FolderRepository;
 import com.efub.clone.googledrive.domain.user.User;
 import com.efub.clone.googledrive.domain.user.UserRepository;
+import com.efub.clone.googledrive.web.dto.FileResponseDto;
 import com.efub.clone.googledrive.web.dto.FileResponseListDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileNotFoundException;
-import java.util.Optional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,77 +24,100 @@ import java.util.stream.Collectors;
 public class FileService {
     private final UserRepository userRepository;
     private final FileRepository fileRepository;
+    private final FolderRepository folderRepository;
     private final S3Service s3Service;
 
     @Transactional
     public String uploadFile(Long userId, MultipartFile file) throws Exception{
         String filepath = s3Service.upload(file);
-        User user = userRepository.findUserByUserId(userId);
-
-        if(user == null){
-            throw new IllegalArgumentException();
-        }
+        CheckInvalidUser(userId);
 
         File entity = File.builder()
                 .filename(file.getOriginalFilename())
                 .type(file.getContentType())
                 .size(file.getSize())
                 .filepath(filepath)
-                .user(user)
+                .user(userRepository.findUserByUserId(userId))
                 .build();
+
         fileRepository.save(entity);
         entity.setOpenedDate();
 
         return "success";
     }
 
+
     @Transactional(readOnly = true)
-    public List<FileResponseListDto> getFiles(Long userId) throws Exception{
-        User user = userRepository.findUserByUserId(userId);
+    public List<FileResponseListDto> getFiles(Long userId) throws IllegalArgumentException {
+        CheckInvalidUser(userId);
 
-        if(user == null){
-            throw new IllegalArgumentException();
-        }
-
-        List<FileResponseListDto> files = fileRepository.findAllByUserUserIdAndDeleteFlag(userId, false)
+        return fileRepository.findAllByUserUserIdAndDeleteFlag(userId, false)
                 .stream()
                 .map(FileResponseListDto::new)
                 .collect(Collectors.toList());
-
-        return files;
     }
 
 
     @Transactional
     public String deleteFile(Long userId, Long fileId) throws Exception{
-        User user = userRepository.findUserByUserId(userId);
+        CheckInvalidUser(userId);
+        CheckInvalidFile(fileId);
 
-        if(user == null) {
-            throw new IllegalArgumentException();
-        }
-
-        File file = fileRepository.getById(fileId);
+        File file = fileRepository.findFileByFileId(fileId);
         file.setDeleteFlag(true);
-
         fileRepository.save(file);
-        return "success";
+
+        return "파일이 휴지통으로 이동되었습니다.";
     }
 
 
     @Transactional
     public String downloadUrl(Long userId, Long fileId) throws FileNotFoundException {
+        CheckInvalidUser(userId);
+        CheckInvalidFile(fileId);
+
+        File file = fileRepository.findFileByFileId(fileId);
+        return file.getFilepath();
+    }
+
+
+    @Transactional
+    public FileResponseDto moveFolder(Long userId, Long fileId, Long folderId) throws Exception {
+        CheckInvalidUser(userId);
+        CheckInvalidFile(fileId);
+        CheckInvalidFolder(folderId);
+
+        File file = fileRepository.findFileByFileId(fileId);
+
+        file.getFolder().setFolderId(folderId);
+        fileRepository.save(file);
+
+        return new FileResponseDto(file);
+    }
+
+
+    // Error Checking function to refactor
+    private void CheckInvalidUser(Long userId) throws IllegalArgumentException {
         User user = userRepository.findUserByUserId(userId);
 
         if(user == null) {
             throw new IllegalArgumentException();
         }
+    }
 
+    private void CheckInvalidFile(Long fileId) throws FileNotFoundException {
         File file = fileRepository.findFileByFileId(fileId);
 
         if(file == null || file.getDeleteFlag()) {
             throw new FileNotFoundException();
         }
+    }
 
-        return file.getFilepath();
+    private void CheckInvalidFolder(Long folderId) throws Exception {
+        Folder folder = folderRepository.findFolderByFolderId(folderId);
+
+        if(folder == null || folder.getDeleteFlag()) {
+            throw new Exception("폴더를 찾을 수 없습니다");
+        }
     }
 }
